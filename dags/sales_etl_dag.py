f87d81250d0fbec16584aa4d6d9325c0fa1230bc
MovingSkeleton <movingskeleton01@gmail.com>
2025-12-10 10:39:54 +0530

    added default args in dag

--- dags/sales_etl_dag.py
+++ dags/sales_etl_dag.py
@@ -2,12 +2,17 @@ from airflow import dags
 from airflow.utils.dates import days_ago
 from airflow.operators.empty import EmptyOperator
 
+default_args={
+    "owner":"Movingskeleton"
+}
+
 with DAG(
     dag_id= 'sales_etl_pipeline'
     start_date=days_ago(1),
     schedule_interval="@daily",
     catchup=False,
     tags=['sales','etl'],
+    default_args=default_args,
 
 ) as dag:
     
